2d1f4ec35c61b046e483b05c9d1115e0230010a3
stefanoreddd <stefanogg95@gmail.com>
2023-04-07 13:00:52 +0200

    price not showing.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-let validDiscountCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+let discountCodesUser = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
 
 
-function workPrice(event){
+function calculateWorkPrice(event){
     event.preventDefault();
+
+    console.log("FUNZIONA!!!!");
+
+    let workPrice = 1;
 
 
     let selectedWorkType = document.getElementById("work-type").value;
@@ -11,41 +15,37 @@ function workPrice(event){
 
     switch(selectedWorkType){
         case "backend":
-            workPrice = 20.50;
+            workPrice *= 20.50;
             break;
         case "frontend":
-            workPrice = 15.30;
+            workPrice *= 15.30;
             break; 
         case "project-analysis":
-            workPrice = 33.60;
+            workPrice *= 33.60;
             break;
     }
 
     let selectedHours = document.getElementById("hoursWork").value;
     selectedHours = parseInt(selectedHours);
 
-    let workPrice = selectedWorkType * selectedHours;
+    workPrice = selectedWorkType * selectedHours;
     workPrice = parseFloat(workPrice);
 
     let discountCode = document.getElementById("coupon").value;
 
-    let isDiscountCodeAvailable = validDiscountCodes.includes(discountCode);
+    let isDiscountCodeAvailable = discountCodesUser.includes(discountCode);
 
     if(isDiscountCodeAvailable){
         workPrice = workPrice * 0,25;
         console.log("Al prezzo è stato inserito un codice sconto del 25%");
-        validDiscountCodes = removeElementFromArray(validDiscountCodes, discountCode);
     } else if (discountCode != ""){
         document.getElementById("coupon-error").innerHTML = "Il codice sconto non è valido!";
         discountCode.classList.add("coupon-red");
     }
 
-    document.getElementById("button").addEventListener("click", workPrice);
-
     document.getElementById("price").innerHTML = "€ " + workPrice.toFixed(2);
 
     
-
 }
 
 
